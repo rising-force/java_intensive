83ebf51245220e49d2c7bb6a90681391f555b15a
lex <aa@bb.ru>
2017-08-26 20:26:16 +0300

    init

---
+++ android/src/ru/geekbrains/java_intensive/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.java_intensive;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.geekbrains.java_intensive.IntensiveGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new IntensiveGame(), config);
+	}
+}
