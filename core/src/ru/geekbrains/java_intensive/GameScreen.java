a48771d287914e4ac3b8a3f78f98f1ebda3b3d21
lex <aa@bb.ru>
2017-08-26 20:48:32 +0300

    1

--- core/src/ru/geekbrains/java_intensive/GameScreen.java
+++ core/src/ru/geekbrains/java_intensive/GameScreen.java
@@ -1,42 +1,67 @@
 package ru.geekbrains.java_intensive;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 class GameScreen implements Screen {
 
+    private SpriteBatch batch;
+    private Texture textureBackground;
+    private Texture textureAsteroid;
+    private Texture textureBtnNewGame;
+
     @Override
     public void show() {
-        System.out.println("show");
+        batch = new SpriteBatch();
+        textureBackground = new Texture("textures/background.png");
+        textureAsteroid = new Texture("textures/asteroid_1.png");
+        textureBtnNewGame = new Texture("textures/btn_new_game.png");
     }
 
     @Override
     public void render(float delta) {
+        update(delta);
+        checkCollision();
+        draw();
+    }
 
+    private void update(float deltaTime) {
+
+    }
+
+    private void checkCollision() {
+
+    }
+
+    private void draw() {
+        Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(textureBackground, 0, 0);
+        batch.end();
     }
 
     @Override
-    public void resize(int width, int height) {
-        System.out.println("resize");
-    }
+    public void resize(int width, int height) {}
 
     @Override
-    public void pause() {
-        System.out.println("pause");
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-        System.out.println("resume");
-    }
+    public void resume() {}
 
     @Override
     public void hide() {
-        System.out.println("hide");
         dispose();
     }
 
     @Override
     public void dispose() {
-        System.out.println("dispose");
+        textureBackground.dispose();
+        textureAsteroid.dispose();
+        textureBtnNewGame.dispose();
     }
 }
--- desktop/src/ru/geekbrains/java_intensive/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/java_intensive/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import ru.geekbrains.java_intensive.IntensiveGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new IntensiveGame(), config);
 	}
 }
