4b8e87cba3b02d040944fdda001f8b7b4e946e35
lex <aa@bb.ru>
2017-08-26 21:04:02 +0300

    1

---
+++ core/src/ru/geekbrains/java_intensive/base/Sprite.java
@@ -0,0 +1,25 @@
+package ru.geekbrains.java_intensive.base;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Sprite extends Rect {
+
+    private TextureRegion textureRegion;
+    private float angle;
+
+    public Sprite(TextureRegion textureRegion) {
+        this.textureRegion = textureRegion;
+        halfWidth = textureRegion.getRegionWidth() / 2f;
+        halfHeight = textureRegion.getRegionHeight() / 2f;
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(
+                textureRegion,
+                getLeft(), getBottom(),
+                halfWidth, halfHeight,
+                getWidth(), getHeight(),
+                1f, 1f, angle);
+    }
+}
